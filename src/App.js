aa86f1bb4032a3a48836cb3b7666df5c4a278e9f
Shaik Habeeb <shaikhabeeb5594@gmail.com>
2023-04-21 00:19:26 +0530

    Change

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@
 import './App.css';
 
 function App() {
+
   return (
+ 
     <div className="App">
-   <h1>hello mr shaik</h1>
-   <h1>i am added some changes</h1>
-   <h1>i tryed push and pull</h1>
+      <h1>try pull and push</h1>
       
+  
     </div>
   );
 }
