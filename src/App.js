2c19b52e6c2c8c8d706ba8a286f04b7d47f593c7
Shaik Habeeb <shaikhabeeb5594@gmail.com>
2023-04-21 00:02:00 +0530

    Added sume chenges

--- src/App.js
+++ src/App.js
@@ -4,10 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Branh_3</h1>
-      <h2>Movie Nmae : Pushpa</h2>
-      <h2>Hero Nmae : Allu Arjun</h2>
-      <h2>Heroine Nmae : Rashmika Mandanna</h2>
+   
       
     </div>
   );
